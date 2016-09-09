f7cdcd4e2a5f2e150e9c68dc776128f98e588120
Alexander01998 <contact.wurstimperium@gmail.com>
2016-09-09 19:00:42 +0200

    Change version to 2.22

--- Wurst Client for MC 1.8.X/src/tk/wurst_client/WurstClient.java
+++ Wurst Client for MC 1.8.X/src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "2.21.2";
+	public static final String VERSION = "2.22";
 	public boolean startupMessageDisabled = false;
 	
 	public AnalyticsManager analytics;
