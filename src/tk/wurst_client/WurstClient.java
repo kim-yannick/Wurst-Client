baf6f917218229f761e2012364e7c96ac1d1b6ff
Alexander01998 <contact.wurstimperium@gmail.com>
2016-12-29 20:45:37 +0100

    Change version to 2.24

--- src/tk/wurst_client/WurstClient.java
+++ src/tk/wurst_client/WurstClient.java
@@ -29,7 +29,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "2.23";
+	public static final String VERSION = "2.24";
 	public static final String MINECRAFT_VERSION = "1.8";
 	
 	public static final NavigableMap<Integer, String> PROTOCOLS;
