dd79e1b169309de40243973c6ba55b8518d47685
Alexander01998 <contact.wurstimperium@gmail.com>
2016-12-30 19:59:09 +0100

    :gem: Clean up AutoRespawnMod

--- src/tk/wurst_client/features/mods/AutoRespawnMod.java
+++ src/tk/wurst_client/features/mods/AutoRespawnMod.java
@@ -1,13 +1,12 @@
 /*
  * Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.client.gui.GuiScreen;
 import tk.wurst_client.events.listeners.DeathListener;
 
 @Mod.Info(description = "Automatically respawns you whenever you die.",
@@ -24,15 +23,15 @@ public class AutoRespawnMod extends Mod implements DeathListener
 	}
 	
 	@Override
-	public void onDeath()
-	{
-		mc.player.respawnPlayer();
-		mc.displayGuiScreen((GuiScreen)null);
-	}
-	
-	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(DeathListener.class, this);
+	}
+	
+	@Override
+	public void onDeath()
+	{
+		mc.player.respawnPlayer();
+		mc.displayGuiScreen(null);
 	}
 }
