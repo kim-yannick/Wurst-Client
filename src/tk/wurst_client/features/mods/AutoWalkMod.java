290024c678d685ecd86d8461eb30097ea3842ab0
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-04 13:17:27 +0100

    :gem: Clean up AutoWalk

--- src/tk/wurst_client/features/mods/AutoWalkMod.java
+++ src/tk/wurst_client/features/mods/AutoWalkMod.java
@@ -1,11 +1,13 @@
 /*
  * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.features.mods;
+
+import org.lwjgl.input.Keyboard;
 
 import tk.wurst_client.events.listeners.UpdateListener;
 
@@ -23,16 +25,19 @@ public class AutoWalkMod extends Mod implements UpdateListener
 	}
 	
 	@Override
-	public void onUpdate()
-	{
-		if(!mc.gameSettings.keyBindForward.pressed)
-			mc.gameSettings.keyBindForward.pressed = true;
-	}
-	
-	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(UpdateListener.class, this);
-		mc.gameSettings.keyBindForward.pressed = false;
+		
+		// reset forward key
+		mc.gameSettings.keyBindForward.pressed =
+			Keyboard.isKeyDown(mc.gameSettings.keyBindForward.getKeyCode());
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		// force-press forward key
+		mc.gameSettings.keyBindForward.pressed = true;
 	}
 }
