0ab45700716d7f94f26b60642bcf7d0975863cb7
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-04 17:05:33 +0100

    Fix AutoFish

--- src/tk/wurst_client/features/mods/AutoFishMod.java
+++ src/tk/wurst_client/features/mods/AutoFishMod.java
@@ -1,66 +1,90 @@
 /*
  * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.entity.projectile.EntityFishHook;
+import net.minecraft.item.ItemFishingRod;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SPacketSoundEffect;
+import tk.wurst_client.events.PacketInputEvent;
+import tk.wurst_client.events.listeners.PacketInputListener;
 import tk.wurst_client.events.listeners.UpdateListener;
+import tk.wurst_client.utils.InventoryUtils;
+import tk.wurst_client.utils.SoundEvents;
 
 @Mod.Info(description = "Automatically catches fish.",
 	name = "AutoFish",
 	tags = "FishBot, auto fish, fish bot, fishing",
 	help = "Mods/AutoFish")
 @Mod.Bypasses
-public class AutoFishMod extends Mod implements UpdateListener
+public class AutoFishMod extends Mod
+	implements UpdateListener, PacketInputListener
 {
-	private boolean catching = false;
+	private int timer;
 	
 	@Override
 	public void onEnable()
 	{
 		wurst.events.add(UpdateListener.class, this);
-	}
-	
-	@Override
-	public void onUpdate()
-	{
-		if(mc.player.fishEntity != null && isHooked(mc.player.fishEntity)
-			&& !catching)
-		{
-			catching = true;
-			mc.rightClickMouse();
-			new Thread("AutoFish")
-			{
-				@Override
-				public void run()
-				{
-					try
-					{
-						Thread.sleep(1000);
-					}catch(InterruptedException e)
-					{
-						e.printStackTrace();
-					}
-					mc.rightClickMouse();
-					catching = false;
-				}
-			}.start();
-		}
+		wurst.events.add(PacketInputListener.class, this);
 	}
 	
 	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(UpdateListener.class, this);
+		wurst.events.remove(PacketInputListener.class, this);
+		timer = 0;
 	}
 	
-	private boolean isHooked(EntityFishHook hook)
+	@Override
+	public void onUpdate()
 	{
-		return hook.motionX == 0.0D && hook.motionZ == 0.0D
-			&& hook.motionY != 0.0D;
+		// check timer
+		if(timer > 0)
+		{
+			timer--;
+			return;
+		}
+		
+		// check bobber
+		if(mc.player.fishEntity != null)
+			return;
+		
+		rightClick();
+	}
+	
+	@Override
+	public void onReceivedPacket(PacketInputEvent event)
+	{
+		// check packet type
+		if(!(event.getPacket() instanceof SPacketSoundEffect))
+			return;
+		
+		// check sound type
+		if(!SoundEvents.ENTITY_BOBBER_SPLASH
+			.equals(((SPacketSoundEffect)event.getPacket()).getSound()))
+			return;
+		
+		rightClick();
+	}
+	
+	private void rightClick()
+	{
+		// check held item
+		ItemStack stack = mc.player.inventory.getCurrentItem();
+		if(InventoryUtils.isEmptySlot(stack)
+			|| !(stack.getItem() instanceof ItemFishingRod))
+			return;
+		
+		// right click
+		mc.rightClickMouse();
+		
+		// reset timer
+		timer = 15;
 	}
 }
--- src/tk/wurst_client/features/mods/PlayerFinderMod.java
+++ src/tk/wurst_client/features/mods/PlayerFinderMod.java
@@ -11,7 +11,7 @@ import java.awt.Color;
 
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S28PacketEffect;
-import net.minecraft.network.play.server.S29PacketSoundEffect;
+import net.minecraft.network.play.server.SPacketSoundEffect;
 import net.minecraft.network.play.server.S2CPacketSpawnGlobalEntity;
 import net.minecraft.util.BlockPos;
 import tk.wurst_client.events.PacketInputEvent;
@@ -87,9 +87,9 @@ public class PlayerFinderMod extends Mod
 			BlockPos pos = effect.func_179746_d();
 			if(BlockUtils.getPlayerBlockDistance(pos) >= 160)
 				blockPos = pos;
-		}else if(packet instanceof S29PacketSoundEffect)
+		}else if(packet instanceof SPacketSoundEffect)
 		{
-			S29PacketSoundEffect sound = (S29PacketSoundEffect)packet;
+			SPacketSoundEffect sound = (SPacketSoundEffect)packet;
 			BlockPos pos = new BlockPos(sound.func_149207_d(),
 				sound.func_149211_e(), sound.func_149210_f());
 			if(BlockUtils.getPlayerBlockDistance(pos) >= 160)
