5e64b686ec7a09b7a84664f1f7e64a7e0002f04c
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-09 17:11:29 +0100

    Update BlockHitMod

--- src/tk/wurst_client/features/mods/BlockHitMod.java
+++ src/tk/wurst_client/features/mods/BlockHitMod.java
@@ -7,25 +7,31 @@
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import tk.wurst_client.events.LeftClickEvent;
 import tk.wurst_client.events.listeners.LeftClickListener;
+import tk.wurst_client.events.listeners.UpdateListener;
+import tk.wurst_client.utils.InventoryUtils;
 
 @Mod.Info(
-	description = "Automatically blocks whenever you hit something with a\n"
-		+ "sword. Some say that you will receive less damage in PVP when doing\n"
-		+ "this.",
+	description = "Automatically blocks whenever you hit something with a sword.\n"
+		+ "Some say that you will receive less damage in PVP when doing this.",
 	name = "BlockHit",
 	tags = "AutoBlock, BlockHitting, auto block, block hitting",
 	help = "Mods/BlockHit")
 @Mod.Bypasses
-public class BlockHitMod extends Mod implements LeftClickListener
+public class BlockHitMod extends Mod
+	implements LeftClickListener, UpdateListener
 {
+	private int timer;
+	
 	@Override
 	public void onEnable()
 	{
+		// reset timer
+		timer = 0;
+		
 		wurst.events.add(LeftClickListener.class, this);
 	}
 	
@@ -33,45 +39,54 @@ public class BlockHitMod extends Mod implements LeftClickListener
 	public void onDisable()
 	{
 		wurst.events.remove(LeftClickListener.class, this);
+		wurst.events.remove(UpdateListener.class, this);
 	}
 	
 	@Override
 	public void onLeftClick(LeftClickEvent event)
 	{
+		// check held item
 		ItemStack stack = mc.player.getCurrentEquippedItem();
+		if(InventoryUtils.isEmptySlot(stack)
+			|| !(stack.getItem() instanceof ItemSword))
+			return;
 		
-		if(stack != null && stack.getItem() instanceof ItemSword)
-			doBlock();
+		doBlock();
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		switch(timer)
+		{
+			case 0:
+				mc.gameSettings.keyBindUseItem.pressed = false;
+				break;
+			
+			case 1:
+			case 2:
+				mc.gameSettings.keyBindUseItem.pressed = true;
+				break;
+			
+			case 3:
+				mc.gameSettings.keyBindUseItem.pressed = false;
+				wurst.events.remove(UpdateListener.class, this);
+				
+				// reset timer
+				timer = -1;
+				break;
+		}
+		
+		// update timer
+		timer++;
 	}
 	
 	public void doBlock()
 	{
-		if(!isActive())
+		// check if ready
+		if(!isActive() || timer > 0)
 			return;
-		new Thread("BlockHit")
-		{
-			@Override
-			public void run()
-			{
-				KeyBinding keybindUseItem = mc.gameSettings.keyBindUseItem;
-				keybindUseItem.pressed = false;
-				try
-				{
-					Thread.sleep(50);
-				}catch(InterruptedException e)
-				{
-					e.printStackTrace();
-				}
-				keybindUseItem.pressed = true;
-				try
-				{
-					Thread.sleep(100);
-				}catch(InterruptedException e)
-				{
-					e.printStackTrace();
-				}
-				keybindUseItem.pressed = false;
-			}
-		}.start();
+		
+		wurst.events.add(UpdateListener.class, this);
 	}
 }
