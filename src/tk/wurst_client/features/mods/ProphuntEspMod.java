0ecebcc192cb1e06a995ff030cb36dc0138f2357
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-25 15:06:36 +0100

    Update ProphuntESP

--- src/tk/wurst_client/features/mods/ProphuntEspMod.java
+++ src/tk/wurst_client/features/mods/ProphuntEspMod.java
@@ -1,17 +1,17 @@
 /*
  * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.features.mods;
 
-import java.awt.Color;
+import org.lwjgl.opengl.GL11;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
 import tk.wurst_client.events.listeners.RenderListener;
 import tk.wurst_client.features.Feature;
@@ -24,11 +24,14 @@ import tk.wurst_client.utils.RenderUtils;
 @Mod.Bypasses
 public class ProphuntEspMod extends Mod implements RenderListener
 {
+	private static final AxisAlignedBB FAKE_BLOCK_BOX =
+		new AxisAlignedBB(-0.5, 0, -0.5, 0.5, 1, 0.5);
+	
 	@Override
 	public Feature[] getSeeAlso()
 	{
-		return new Feature[]{wurst.mods.playerEspMod,
-			wurst.mods.mobEspMod, wurst.mods.tracersMod};
+		return new Feature[]{wurst.mods.playerEspMod, wurst.mods.mobEspMod,
+			wurst.mods.tracersMod};
 	}
 	
 	@Override
@@ -38,33 +41,60 @@ public class ProphuntEspMod extends Mod implements RenderListener
 	}
 	
 	@Override
-	public void onRender()
-	{
-		for(Object entity : mc.world.loadedEntityList)
-			if(entity instanceof EntityLiving && ((Entity)entity).isInvisible())
-			{
-				double x = ((Entity)entity).posX;
-				double y = ((Entity)entity).posY;
-				double z = ((Entity)entity).posZ;
-				Color color;
-				if(mc.player.getDistanceToEntity((Entity)entity) >= 0.5)
-					color =
-						new Color(1F, 0F, 0F,
-							0.5F - MathHelper
-								.abs(MathHelper
-									.sin(Minecraft.getSystemTime() % 1000L
-										/ 1000.0F * (float)Math.PI * 1.0F)
-									* 0.3F));
-				else
-					color = new Color(0, 0, 0, 0);
-				RenderUtils.box(x - 0.5, y - 0.1, z - 0.5, x + 0.5, y + 0.9,
-					z + 0.5, color);
-			}
-	}
-	
-	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(RenderListener.class, this);
+	}
+	
+	@Override
+	public void onRender()
+	{
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-mc.getRenderManager().renderPosX,
+			-mc.getRenderManager().renderPosY,
+			-mc.getRenderManager().renderPosZ);
+		
+		// set color
+		float alpha = 0.5F + 0.25F * MathHelper
+			.sin(System.currentTimeMillis() % 1000 / 500F * (float)Math.PI);
+		GL11.glColor4f(1, 0, 0, alpha);
+		
+		// draw boxes
+		for(Entity entity : mc.world.loadedEntityList)
+		{
+			if(!(entity instanceof EntityLiving))
+				continue;
+			
+			if(!entity.isInvisible())
+				continue;
+			
+			if(mc.player.getDistanceSqToEntity(entity) < 0.25)
+				continue;
+			
+			GL11.glPushMatrix();
+			GL11.glTranslated(entity.posX, entity.posY, entity.posZ);
+			
+			RenderUtils.drawOutlinedBox(FAKE_BLOCK_BOX);
+			RenderUtils.drawSolidBox(FAKE_BLOCK_BOX);
+			
+			GL11.glPopMatrix();
+		}
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 }
