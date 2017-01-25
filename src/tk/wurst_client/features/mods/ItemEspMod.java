7412a49599549c5ee3007e0d981feba464e8b950
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-25 09:47:16 +0100

    Update ItemESP

--- src/tk/wurst_client/features/mods/ItemEspMod.java
+++ src/tk/wurst_client/features/mods/ItemEspMod.java
@@ -1,14 +1,17 @@
 /*
  * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.features.mods;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.util.AxisAlignedBB;
 import tk.wurst_client.events.listeners.RenderListener;
 import tk.wurst_client.features.Feature;
 import tk.wurst_client.utils.RenderUtils;
@@ -20,6 +23,9 @@ import tk.wurst_client.utils.RenderUtils;
 @Mod.Bypasses
 public class ItemEspMod extends Mod implements RenderListener
 {
+	private static final AxisAlignedBB ITEM_BOX =
+		new AxisAlignedBB(-0.175, 0, -0.175, 0.175, 0.35, 0.175);
+	
 	@Override
 	public Feature[] getSeeAlso()
 	{
@@ -33,16 +39,48 @@ public class ItemEspMod extends Mod implements RenderListener
 	}
 	
 	@Override
-	public void onRender()
-	{
-		for(Object entity : mc.world.loadedEntityList)
-			if(entity instanceof EntityItem)
-				RenderUtils.entityESPBox((Entity)entity, 2);
-	}
-	
-	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(RenderListener.class, this);
+	}
+	
+	@Override
+	public void onRender()
+	{
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-mc.getRenderManager().renderPosX,
+			-mc.getRenderManager().renderPosY,
+			-mc.getRenderManager().renderPosZ);
+		
+		GL11.glColor4d(1, 1, 0, 0.5F);
+		
+		// draw boxes
+		for(Entity entity : mc.world.loadedEntityList)
+			if(entity instanceof EntityItem)
+			{
+				GL11.glPushMatrix();
+				GL11.glTranslated(entity.posX, entity.posY, entity.posZ);
+				
+				RenderUtils.drawOutlinedBox(ITEM_BOX);
+				
+				GL11.glPopMatrix();
+			}
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 }
