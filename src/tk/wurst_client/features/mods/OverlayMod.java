d162c8347b8ebddd014e288a4acec233a87b9fda
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-28 17:12:51 +0100

    Update Overlay

--- src/tk/wurst_client/features/mods/OverlayMod.java
+++ src/tk/wurst_client/features/mods/OverlayMod.java
@@ -1,15 +1,15 @@
 /*
  * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.block.Block;
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.RayTraceResult.Type;
 import tk.wurst_client.events.listeners.RenderListener;
 import tk.wurst_client.features.Feature;
 import tk.wurst_client.utils.RenderUtils;
@@ -33,21 +33,66 @@ public class OverlayMod extends Mod implements RenderListener
 	}
 	
 	@Override
-	public void onRender(float partialTicks)
-	{
-		if(mc.objectMouseOver == null
-			|| mc.objectMouseOver.typeOfHit != Type.BLOCK)
-			return;
-		BlockPos pos = mc.objectMouseOver.getBlockPos();
-		Block mouseOverBlock =
-			mc.world.getBlockState(mc.objectMouseOver.getBlockPos()).getBlock();
-		if(Block.getIdFromBlock(mouseOverBlock) != 0)
-			RenderUtils.nukerBox(pos, mc.playerController.curBlockDamageMP);
-	}
-	
-	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(RenderListener.class, this);
+	}
+	
+	@Override
+	public void onRender(float partialTicks)
+	{
+		// check hitResult
+		if(mc.objectMouseOver == null)
+			return;
+		
+		// check position
+		BlockPos pos = mc.objectMouseOver.getBlockPos();
+		if(pos == null)
+			return;
+		
+		// check if hitting block
+		if(!mc.playerController.getIsHittingBlock())
+			return;
+		
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-mc.getRenderManager().renderPosX,
+			-mc.getRenderManager().renderPosY,
+			-mc.getRenderManager().renderPosZ);
+		
+		// set position
+		GL11.glTranslated(pos.getX(), pos.getY(), pos.getZ());
+		
+		// set size
+		GL11.glTranslated(0.5, 0.5, 0.5);
+		double v = mc.playerController.curBlockDamageMP;
+		GL11.glScaled(v, v, v);
+		GL11.glTranslated(-0.5, -0.5, -0.5);
+		
+		// get color
+		float red = mc.playerController.curBlockDamageMP * 2F;
+		float green = 2 - red;
+		
+		// draw box
+		GL11.glColor4f(red, green, 0, 0.25F);
+		RenderUtils.drawSolidBox();
+		GL11.glColor4f(red, green, 0, 0.5F);
+		RenderUtils.drawOutlinedBox();
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 }
