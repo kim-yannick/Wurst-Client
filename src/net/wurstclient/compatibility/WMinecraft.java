8e03a6787c8d7bb4a9836c4b9aedd7b0a90267bf
Alexander01998 <contact.wurstimperium@gmail.com>
2017-04-05 20:43:19 +0200

    Add WMinecraft.getConnection()

--- src/net/wurstclient/compatibility/WMinecraft.java
+++ src/net/wurstclient/compatibility/WMinecraft.java
@@ -7,6 +7,7 @@ import java.util.TreeMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.client.network.NetHandlerPlayClient;
 
 public final class WMinecraft
 {
@@ -32,6 +33,11 @@ public final class WMinecraft
 	public static WorldClient getWorld()
 	{
 		return mc.theWorld;
+	}
+	
+	public static NetHandlerPlayClient getConnection()
+	{
+		return getPlayer().connection;
 	}
 	
 	public static boolean isRunningOnMac()
